a45f6b8ffd3ec3ae0b4a0fd7cdea7fc4e1021578
Ubaid Sheikh <md.ubaid.sheikh@gmail.com>
2025-04-20 10:07:55 +0000

    Submit Referral Request button does not procced to add data into database

--- src/app/request-referral/page.tsx
+++ src/app/request-referral/page.tsx
@@ -11,10 +11,8 @@ import { Textarea } from "@/components/ui/textarea";
 import { toast } from "@/hooks/use-toast";
 import { uploadFile } from "@/services/file-upload";
 import { sendEmail } from "@/services/email";
-import { initFirestore, firebaseApp } from "@/services/firebase";
+import { firebaseApp, initFirestore } from "@/services/firebase";
 import { useRouter } from 'next/navigation';
-import { Home } from "lucide-react";
-import Link from "next/link";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const formSchema = z.object({
@@ -125,9 +123,6 @@ const RequestReferralPage = () => {
 
   return (
     <div className="container mx-auto py-10">
-      <Link href="/" className="absolute top-4 left-4 text-2xl font-bold text-primary flex items-center">
-        <Home className="inline-block h-6 w-6 mr-2" />
-      </Link>
       <h1 className="text-2xl font-bold mb-4">Referral Request Form</h1>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
